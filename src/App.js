8fb91cf87df91ad9cc3f1ee5d5331711e2aeec8d
jlamb88 <112279704+jlamb88@users.noreply.github.com>
2023-09-18 15:42:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const App = () => (
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Feed />} />
-        <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/channel/:id" element={<ChannelDetail />} />
-        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route exact path="/yt-lite/" element={<Feed />} />
+        <Route path="/yt-lite/video/:id" element={<VideoDetail />} />
+        <Route path="/yt-lite/channel/:id" element={<ChannelDetail />} />
+        <Route path="/yt-lite/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
